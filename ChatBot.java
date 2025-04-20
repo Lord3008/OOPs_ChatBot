921a927b533c50c514745170c3eaffdf16209409
Lord Sen <157342276+Lord3008@users.noreply.github.com>
2025-04-20 22:48:52 +0530

    Added Chatbot files using Java Swing

---
+++ ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+public class ChatBot {
+
+	public static void main(String[] args) {
+		ChatBotMain obj=new ChatBotMain();
+		obj.setTitle("ChatBot(Lord Sen)");
+		obj.setVisible(true);
+	}
+}
